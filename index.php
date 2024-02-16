efbc2e5a894a251624b85927b0955d35170eee3e
Alexsander Klinkovsky <alexsander.kapitel@gmail.com>
2024-02-16 12:36:01 +0300

    Внесены изменения в index.php

--- index.php
+++ index.php
@@ -2,25 +2,3 @@
 declare(strict_types=1);
 
 include 'form.html';
-
-
-
-
-
-
-
-
-/*
-session_start();
-
-if (isset($_SESSION['name'])) {
-    echo sprintf('Привет %s', $_SESSION['name']);
-    echo "<br>";
-    echo "<br>";
-    echo '<a href="exit.php">Войти под другим именем!</a> <br>';
-    echo "<br>";
-}
-else {
-    include 'form.html';
-}
-*/
